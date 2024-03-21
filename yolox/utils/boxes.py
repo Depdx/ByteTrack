a1a96b10fb55958d054285746eefee7edb8557bf
Nicolas Depelteau <nicolas@depelteau.ca>
2024-03-21 12:30:39 -0400

    Fix indexing bug in postprocess function

--- yolox/utils/boxes.py
+++ yolox/utils/boxes.py
@@ -46,7 +46,7 @@ def postprocess(prediction, num_classes, conf_thre=0.7, nms_thre=0.45):
             continue
         # Get score and class with highest confidence
         print(image_pred.shape)
-        class_conf, class_pred = torch.max(image_pred[:, 47], 1, keepdim=True)
+        class_conf, class_pred = torch.max(image_pred[:, 47:47], 1, keepdim=True)
 
         conf_mask = (image_pred[:, 4] * class_conf.squeeze() >= conf_thre).squeeze()
         # _, conf_mask = torch.topk((image_pred[:, 4] * class_conf.squeeze()), 1000)
