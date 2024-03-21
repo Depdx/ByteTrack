6dddca4db692a9bdf1194f4dbdb9cf09d94252ef
Nicolas Depelteau <nicolas@depelteau.ca>
2024-03-21 12:41:42 -0400

    Refactor class_conf and class_pred calculation in postprocess function

--- yolox/utils/boxes.py
+++ yolox/utils/boxes.py
@@ -45,9 +45,7 @@ def postprocess(prediction, num_classes, conf_thre=0.7, nms_thre=0.45):
         if not image_pred.size(0):
             continue
         # Get score and class with highest confidence
-        class_conf, class_pred = torch.max(
-            image_pred[:, 47].unsqueeze(1), 1, keepdim=True
-        )
+        class_conf, class_pred = torch.max(image_pred[:, [5, 47]], 1, keepdim=True)
 
         conf_mask = (image_pred[:, 4] * class_conf.squeeze() >= conf_thre).squeeze()
         # _, conf_mask = torch.topk((image_pred[:, 4] * class_conf.squeeze()), 1000)
